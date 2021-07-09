7f57c4ac89033f555490c1deb9d2b7904fbbb50f
HiImHibiki <mario.rufisanto@gmail.com>
2021-07-10 04:14:31 +0700

    date updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var d = document.querySelector('.countdown-days');
 var h = document.querySelector('.countdown-hours');
 var m = document.querySelector('.countdown-minutes');
 var s = document.querySelector('.countdown-seconds');
+var mil = document.querySelector('.countdown-miliseconds');
 
 var targetDate = new Date('07/11/2021 23:00:00').getTime();
 console.log(targetDate);
@@ -19,13 +20,18 @@ var timeSet = (targetDate) => {
     var textHour = Math.floor((diff % day) / hour);
     var textMinute = Math.floor((diff % hour) / minute);
     var textSecond = Math.floor((diff % minute) / second);
+    
+    var date = new Date()
+    var currHour = date.getHours();
+    var currMin = date.getMinutes();
+    var currSec = date.getSeconds();
 
 
     // console.log(textDay.toString().padStart(2, '0'), textHour, textMinute, textSecond);
-    d.innerHTML = textDay.toString().padStart(2, '0');
-    h.innerHTML = textHour.toString().padStart(2, '0');
-    m.innerHTML = textMinute.toString().padStart(2, '0');
-    s.innerHTML = textSecond.toString().padStart(2, '0');
+    // d.innerHTML = textDay.toString().padStart(2, '0');
+    h.innerHTML = currHour.toString().padStart(2, '0');
+    m.innerHTML = currMin.toString().padStart(2, '0');
+    s.innerHTML = currSec.toString().padStart(2, '0');
 };
 
 setInterval(() => {
